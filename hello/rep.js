03b1b54c07166274fdb918df115ae71209e9fc27
Pedro Henrique <pedrinfnn@gmail.com>
2022-08-09 01:01:08 -0300

    :fire:

---
+++ hello/rep.js
@@ -0,0 +1,18 @@
+console.log("imprimindo de 1 a 10 com while");
+var contador1 = 1;
+while (contador1 <= 10) {
+  console.log(contador1);
+  contador1++;
+}
+console.log("imprimindo de 1 a 7 com dowhile");
+contador2 = 1;
+do {
+  console.log(contador2);
+  contador2++;
+} while (contador2 <= 7);
+
+console.log("imprimindo de 1 a 5 com for");
+for (var contador3 = 1; contador3 <= 5; contador3++) {
+  console.log(contador3);
+  break;
+}
