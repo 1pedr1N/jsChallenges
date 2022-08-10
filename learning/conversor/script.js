f66f230cc8198e4de855fa8fbf02504ab2b692cf
Pedro Henrique <pedrinfnn@gmail.com>
2022-08-10 17:51:56 -0300

    :fire:

---
+++ learning/conversor/script.js
@@ -0,0 +1,25 @@
+function DayToMinutes(x) {
+  return x * 24 * 60;
+}
+function DayToHours(x) {
+  return x * 24;
+}
+function DayToSeconds(x) {
+  return x * 24 * 60 * 60;
+}
+function DayToMiliseconds(x) {
+  return x * 24 * 60 * 60 * 1000;
+}
+
+function MinutesToDay(x) {
+  return x / (24 * 60);
+}
+function HoursToDay(x) {
+  return x / 24;
+}
+function SecondsToDay(x) {
+  return x / (24 * 60 * 60);
+}
+function MilisecondsToDay(x) {
+  return x / (24 * 60 * 60 * 1000);
+}
