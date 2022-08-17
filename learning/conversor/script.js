2b383eaa19e9bf8532b3c9d0de2c27f97d308332
Pedro Henrique <pedrinfnn@gmail.com>
2022-08-17 15:27:51 -0300

    :fire:

--- learning/conversor/script.js
+++ learning/conversor/script.js
@@ -23,3 +23,30 @@ function SecondsToDay(x) {
 function MilisecondsToDay(x) {
   return x / (24 * 60 * 60 * 1000);
 }
+function MlToLiters(x) {
+  return x / 1000;
+}
+function LitersToMl(x) {
+  return x * 1000;
+}
+function DollarToReal(x) {
+  return x * 5.14;
+}
+function RealToDollar(x) {
+  return x / 5.14;
+}
+
+function Apresetantion(name = "", age = "", country = "") {
+  return `Meu nome é ${name}, Eu tenho ${age} anos e moro em ${country}`;
+}
+function ganhoPorHOra(salario, horas) {
+  const ganho = salario / horas;
+  return Math.round(ganho);
+}
+function ganhoPorHoraBRL(salario, horas) {
+  const ganho = salario / horas;
+  return ganho.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
